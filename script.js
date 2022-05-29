26659075d0837cc51bb65b0d539668dc221b2109
blees12345 <adriang1702@gmail.com>
2022-05-28 22:29:42 -0700

    added stickty nav bar

--- script.js
+++ script.js
@@ -19,9 +19,38 @@ closeEmail.onclick = function(e){
 	e.preventDefault();
 	modal.style.display = "none";
 	userEmail = document.querySelector('.emailinput').value;
+	userMessege = document.querySelector('.messege').value;
+
 }
 // save email as variable
 closeEmail.addEventListener('click', function(){
-	userEmail 
+	userEmail;
+	userMessege;
+
+	console.log(userMessege);
 	console.log(userEmail)
+
 })
+
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+	const currentScroll = window.pageYOffset;
+	if (currentScroll <= 0) {
+		body.classList.remove('scroll-up');
+		return;
+	}
+
+	if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+		body.classList.remove('scroll-up');
+		body.classList.add('scroll-down');
+	} else if (
+		currentScroll < lastScroll &&
+		body.classList.contains('scroll-down')
+	) {
+		body.classList.remove('scroll-down');
+		body.classList.add('scroll-up');
+	}
+	lastScroll = currentScroll;
+});
